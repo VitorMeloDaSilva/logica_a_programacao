47d988fb11d93384170b2839a98f943adc198f85
VitorMeloDaSilva <94999881+VitorMeloDaSilva@users.noreply.github.com>
2021-12-18 08:09:22 -0300

    Create exercício3.py

---
+++ exercício3.py
@@ -0,0 +1,9 @@
+    #EXERCICIO 3
+
+#3) Calcular e apresentar o valor do volume de uma lata de óleo, utilizando a fórmula:
+#Volume ← π * Raio2 * Altura
+raio = float(input("digite o valor do raio:"))
+altura = float(input("digite o valor da autura:"))
+pi = float(3.14)
+volume = pi * raio ** 2 * altura 
+print(f"o valor do volume è:{volume}")
