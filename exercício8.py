cb143c81617eaf5b5b82c8e10c3c9116fc4576c8
VitorMeloDaSilva <94999881+VitorMeloDaSilva@users.noreply.github.com>
2021-12-18 08:11:59 -0300

    Create exercício8.py

---
+++ exercício8.py
@@ -0,0 +1,10 @@
+   #EXERCICIO 8
+
+#8) Elaborar um programa que calcule e apresente o volume de uma caixa retangular, por meio da
+#fórmula:
+#VOLUME ← COMPRIMENTO * LARGURA * ALTURA.
+comprimento = float((input("infrome o comprimento:\n")))
+largura = float((input("infrome a largura:\n")))
+altura = float((input("infrome a altura:\n")))
+volume = comprimento * largura * altura
+print(f"o volume é:\n{volume}")
