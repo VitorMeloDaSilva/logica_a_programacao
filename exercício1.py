64e9c32f432b0fc971d3e7512a494b30fa863274
VitorMeloDaSilva <94999881+VitorMeloDaSilva@users.noreply.github.com>
2021-12-18 08:06:29 -0300

    Create exercício1.py

---
+++ exercício1.py
@@ -0,0 +1,8 @@
+   #EXRCICIO 1
+
+#1) Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. A fórmula
+#de conversão é F ← (9 * C + 160) / 5, sendo F a temperatura em Fahrenheit e C a temperatura em
+#Celsius.
+Celsius = float(input("qual a temperatura que você quer transformar?")) 
+Fahrenheit = ( 9 * Celsius + 160 ) /5 
+print(f"a temperatura em Fahrenheit è:. {Fahrenheit}") 
