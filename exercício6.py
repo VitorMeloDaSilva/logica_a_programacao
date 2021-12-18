9f75c7adc9fcde1bbf52db26156d1b12f33f3df6
VitorMeloDaSilva <94999881+VitorMeloDaSilva@users.noreply.github.com>
2021-12-18 08:11:03 -0300

    Create exercício6.py

---
+++ exercício6.py
@@ -0,0 +1,9 @@
+   #EXERCICIO 6
+
+#6) Ler dois valores (inteiros, reais ou caracteres) para as variáveis A e B, e efetuar a troca dos
+#valores de forma que a variável A passe a possuir o valor da variável B e a variável B passe a
+#possuir o valor da variável A. Apresentar os valores trocados
+a = float (input("digite o valor de a:\n"))
+b = float(input("digite o valor de b:\n"))
+print(f"o valor de a:\n{b}")
+print(f"o valor de b:\n{a}")
