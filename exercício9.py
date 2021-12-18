3a744e5619c3e3d38a8b113fa7c0e70a7fb70c5c
VitorMeloDaSilva <94999881+VitorMeloDaSilva@users.noreply.github.com>
2021-12-18 08:12:47 -0300

    Create exercício9.py

---
+++ exercício9.py
@@ -0,0 +1,9 @@
+  #EXERCICIO 9
+
+#9) Ler dois inteiros (variáveis A e B) e imprimir o resultado do quadrado da diferença do primeiro
+#valor pelo segundo.
+a = int(input("digite o valor de a:\n"))
+b = int(input("digite o valor de b:\n"))
+d = a - b 
+q = d **2 
+print(f'o quadrado da diferença de a e b é:\n{q}')
